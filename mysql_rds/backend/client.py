db1e544aaa5453a3214ae7ad8289d8ae572a3b46
Chris Ramshaw <chris.ramshaw@nanoporetech.com>
2020-12-03 14:28:17 +0000

    bump django to 3.1.x

--- mysql_rds/backend/client.py
+++ mysql_rds/backend/client.py
@@ -4,8 +4,8 @@ from django.db.backends.mysql.client import DatabaseClient as MySQLClient
 class DatabaseClient(MySQLClient):
 
     @classmethod
-    def settings_to_cmd_args(cls, settings_dict):
+    def settings_to_cmd_args(cls, settings_dict, parameters):
 
         if callable(settings_dict['PASSWORD']):
             settings_dict['PASSWORD'] = settings_dict['PASSWORD']()
-        return super().settings_to_cmd_args(settings_dict)
+        return super().settings_to_cmd_args(settings_dict, parameters)
